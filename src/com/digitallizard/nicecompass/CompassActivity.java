85142456b29b22d19b4df313565114980623e7c1
Oscar Key <oscar@keyfamily.org.uk>
2011-08-06 21:55:19 +0100

    Removed unecessary setting files and added true north persistancy.

--- src/com/digitallizard/nicecompass/CompassActivity.java
+++ src/com/digitallizard/nicecompass/CompassActivity.java
@@ -7,6 +7,8 @@
 package com.digitallizard.nicecompass;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,11 +16,21 @@ import android.view.Window;
 import android.widget.LinearLayout;
 
 public class CompassActivity extends Activity {
+	public static final String PREF_FILE_NAME = "com.digitallizard.nicecompass_preferences";
+	public static final String PREFKEY_USE_TRUE_NORTH = "useTrueNorth";
+	public static final boolean DEFAULT_USE_TRUE_NORTH = true;
+	
 	private CompassManager compass;
 	private CompassSurface surface;
 	private LinearLayout surfaceContainer;
 
 	public void onPause() {
+		// save the current north state
+		SharedPreferences settings = this.getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE);
+		Editor editor = settings.edit();
+		editor.putBoolean(PREFKEY_USE_TRUE_NORTH, surface.useTrueNorth());
+		editor.commit();
+		
 		// unregister from the compass to prevent undue battery drain
 		compass.unregisterSensors();
 		// stop the animation
@@ -43,9 +55,13 @@ public class CompassActivity extends Activity {
         // create the gui
         setContentView(R.layout.main);
         
+        // load in the settings
+        SharedPreferences settings = this.getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE);
+        boolean useTrueNorth = settings.getBoolean(PREFKEY_USE_TRUE_NORTH, DEFAULT_USE_TRUE_NORTH);
+        
         // initialize variables
         compass = new CompassManager(this);
-        surface = new CompassSurface(this, compass, true);
+        surface = new CompassSurface(this, compass, useTrueNorth);
         surfaceContainer = (LinearLayout)findViewById(R.id.compassSurfaceContainer);
         
         // add the compass
