88f4770600501ffc6d0d171f99bc179c82b2b23a
Oscar Key <oscar@keyfamily.org.uk>
2011-08-23 19:59:04 +0100

    Removed uneeded imports.

--- src/com/digitallizard/nicecompass/CompassSurface.java
+++ src/com/digitallizard/nicecompass/CompassSurface.java
@@ -2,14 +2,12 @@ package com.digitallizard.nicecompass;
 
 import java.text.DecimalFormat;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.GradientDrawable.Orientation;
